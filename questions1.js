961bac7616ba9a77d03a3f4cbee58dc7f93c8795
Achaiahb <148974116+Achaiahb@users.noreply.github.com>
2023-10-25 19:13:13 +0530

    Add files via upload

---
+++ questions1.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the primary purpose of python?",
+    answer: "Data analysis and scientific computing",
+    options: [
+      "Web development",
+      "Data analysis and scientific computing",
+      "Game development",
+      "Mobile app development"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which programming language is known for its aritificial intelligence?",
+    answer: "Python",
+    options: [
+      "Java",
+      "Ruby",
+      "Python",
+      "c++"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In which language is Javascript primarily used?",
+    answer: "Front-end development",
+    options: [
+      "Server-side Scripting",
+      "Mobile app development",
+      "Front-end development",
+      "Game development"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which programming language is known for its simplicity?",
+    answer: "Python",
+    options: [
+      "Python",
+      "Java",
+      "C",
+      "C++"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the main purpose of SQL in the context of programming?",
+    answer: "Database querying",
+    options: [
+      "Database querying",
+      "Front-end",
+      "Designing",
+      "Game development"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ questions2.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the primary goal of AI?",
+    answer: "Replicating human intelligence",
+    options: [
+      "Replicating human intelligence",
+      "Automating all tasks",
+      "Developing advanced robots",
+      "Mobile app development"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which technique in AI allows computers to improve their performance on a specific tasks?",
+    answer: "Neural networks",
+    options: [
+      "Neural networks",
+      "Genetic algorithms",
+      "Algorithms Programming",
+      "Quantum computing"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Expansion of NLP",
+    answer: "Natural language processing",
+    options: [
+      "Natural learning process",
+      "Natural language processing",
+      "Neural language processing",
+      "Natural language preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which AI subfield is focused on making decisions in uncertain environment?",
+    answer: "Reinforcement learning",
+    options: [
+      "Machine learning",
+      "Expert systems",
+      "Reinforcement learning",
+      "Robotics"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does the turing test assess in the context of AI?",
+    answer: "Conversational ability and human-likeness of AI",
+    options: [
+      "Processing speed of AI systems",
+      "Emotional intelligence of AI",
+      "Conversational ability and human-likeness of AI",
+      "Memory capacity of AI"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
